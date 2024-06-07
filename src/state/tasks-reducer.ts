253e454ff7f63279f19c33ec18e9c424f11dbf9c
Evgeniy Klimovch <kliasanik@gmail.com>
2024-06-07 18:13:15 +0300

    tasks-reducer.ts

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,5 +1,4 @@
-import {FilterValuesType, TasksStateType, TodolistType} from "../App";
-import {v1} from "uuid";
+import {TasksStateType} from "../App";
 
 type TasksReducerType =
    removeTaskACType
